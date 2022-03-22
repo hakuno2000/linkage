6440e599e40f9ddab9accefbc2101046bd4be5a1
Nguyễn Quang Vinh <hakuno2000@gmail.com>
2022-03-22 14:05:30 +0700

    Initial commit

---
+++ src/test/java/com/bglobal/linkage/LinkageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bglobal.linkage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
