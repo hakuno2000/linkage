c644ec115325dd532ef37216ed5ef951ed8950ab
Nguyễn Quang Vinh <hakuno2000@gmail.com>
2022-07-24 17:00:06 +0700

    add test

--- src/test/java/com/bglobal/linkage/LinkageApplicationTests.java
+++ src/test/java/com/bglobal/linkage/LinkageApplicationTests.java
@@ -1,13 +1,36 @@
 package com.bglobal.linkage;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.io.IOException;
+import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest
 class LinkageApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void givenUserDoesNotExists_whenUserInfoIsRetrieved_then404IsReceived()
+            throws ClientProtocolException, IOException {
+
+        // Given
+        String categoryCode = "277_1000370";
+        String shopCode = "277_404";
+        HttpUriRequest request = new HttpGet( "https://localhost:8080/category/read?categoryCode=" + categoryCode + "&shopCode=" + shopCode);
+
+        // When
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
+
+        // Then
+        Assertions.assertEquals(
+                httpResponse.getStatusLine().getStatusCode(),
+                equalTo(HttpStatus.SC_OK));
     }
 
 }
